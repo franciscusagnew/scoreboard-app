18e4a69196ff142f384e2816f4e40bd8b5092e88
Franciscus Agnew <agnew_f@hotmail.com>
2018-03-09 10:41:34 -0800

    Updated app.jsx

--- app.jsx
+++ app.jsx
@@ -1,12 +1,29 @@
-function App() {
-	return (
-		<div>
-			<h1>Hello from React</h1>
-			<p>This is rendered from the App component.</p>
-		</div>
-	);
+function Application() {
+  return(
+    <div className="scoreboard">
+        <div className="header">
+            <h1>Scoreboard</h1>
+        </div>
+        <div className="players">
+            <div className="player">
+                <div className="player-name">
+                    Franciscus Agnew
+                </div>
+                <div className="player-score">
+                    <div className="counter">
+                        <button className="counter-action decrement"> - </button>
+                        <div className="counter-score"> 31 </div>
+                        <button className="counter-action increment"> + </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
 
-ReactDOM.render(
-	<App />, document.getElementById('container')
-);
+let target = document.getElementById('container');
+
+// ReactDOM.render(<h1>Hello, World!</h1>, target);
+
+ReactDOM.render(<Application/>, target);
