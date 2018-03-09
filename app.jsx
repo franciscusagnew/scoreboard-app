ac98f8e84c05d3ad232829f4b876ff148118832d
Franciscus Agnew <agnew_f@hotmail.com>
2018-03-09 11:12:05 -0800

    Updated app.jsx

--- app.jsx
+++ app.jsx
@@ -1,28 +1,18 @@
 function Application(props) {
   return(
-  	// .scoreboard
     <div className="scoreboard">
-    	// .header
         <div className="header">
             <h1>{props.title}</h1>
         </div>
-        // .players
         <div className="players">
-        	// .player
             <div className="player">
-            	// .player-name
                 <div className="player-name">
                     Franciscus Agnew
                 </div>
-                // .player-score
                 <div className="player-score">
-                	// .counter
                     <div className="counter">
-                    	// .counter-action .decrement
                         <button className="counter-action decrement"> - </button>
-                        // .counter-score
                         <div className="counter-score"> 31 </div>
-                        // .counter-action .increment
                         <button className="counter-action increment"> + </button>
                     </div>
                 </div>
