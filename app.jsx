fbce7eb209ac4f44af85baeeaf616538645f7bd7
Franciscus Agnew <agnew_f@hotmail.com>
2018-03-09 12:23:52 -0800

    Decomposed app.jsx into Header, Player, and Counter components

--- app.jsx
+++ app.jsx
@@ -1,22 +1,61 @@
+// Add a Header component
+function Header(props) {
+	return(
+		<div className="header">
+            <h1>{props.title}</h1>
+        </div>
+	);
+}
+
+Header.propTypes = {
+	// Use isRequired function to check if property exists during runtime
+	title: React.PropTypes.string.isRequired,
+};
+
+// Add a Counter component
+function Counter(props) {
+	return(
+		<div className="counter">
+	        <button className="counter-action decrement"> - </button>
+	        <div className="counter-score"> {props.score} </div>
+	        <button className="counter-action increment"> + </button>
+	    </div>
+    );
+}
+
+Counter.propTypes = {
+	// Use isRequired function to check if property exists during runtime
+	score: React.PropTypes.number.isRequired,
+};
+
+// Add a Player component
+function Player(props) {
+	return(
+		<div className="player">
+	        <div className="player-name">
+	        	{props.name}
+	        </div>
+	        <div className="player-score">
+	            <Counter score={props.score} />
+	        </div>
+	    </div>
+	);
+}
+
+Player.propTypes = {
+	// Use isRequired function to check if property exists during runtime
+	name: React.PropTypes.string.isRequired,
+	score: React.PropTypes.number.isRequired,
+};
+
+
+// Add Application component
 function Application(props) {
   return(
     <div className="scoreboard">
-        <div className="header">
-            <h1>{props.title}</h1>
-        </div>
+        <Header title={props.title} />
         <div className="players">
-            <div className="player">
-                <div className="player-name">
-                    Franciscus Agnew
-                </div>
-                <div className="player-score">
-                    <div className="counter">
-                        <button className="counter-action decrement"> - </button>
-                        <div className="counter-score"> 31 </div>
-                        <button className="counter-action increment"> + </button>
-                    </div>
-                </div>
-            </div>
+            <Player name="Franciscus Agnew" score={12} />
         </div>
     </div>
   );
