b804cb973abd575ce0e6a08ef097a976b59bb4b3
Franciscus Agnew <agnew_f@hotmail.com>
2018-03-13 03:48:39 -0700

    Defined a Stats component, updated the Header component to displaye total number of players and points

--- app.jsx
+++ app.jsx
@@ -1,4 +1,4 @@
-// Declare an array of Players
+// Define an array of Players
 var PLAYERS = [
 	{
 		name: "Franciscus Agnew",
@@ -15,37 +15,70 @@ var PLAYERS = [
 		score: 60,
 		id: 3,
 	},
-]
+];
 
-// Add a Header component
+// Define Stats component
+function Stats(props) {
+	var totalPlayers = props.players.length;
+	var totalPoints = props.players.reduce(function(total, player){
+		return total + player.score;
+	}, 0);
+
+	return (
+		<table className="stats">
+			<tbody>
+				<tr>
+					<td>Player:</td>
+					<td>{totalPlayers}</td>
+				</tr>
+				<tr>
+					<td>Total Points:</td>
+					<td>{totalPoints}</td>
+				</tr>
+			</tbody>
+		</table>
+	)
+}
+
+Stats.propTypes = {
+	players: React.PropTypes.array.isRequired,
+};
+
+// Define Header component
 function Header(props) {
 	return(
 		<div className="header">
+			<Stats players={props.players}/>
       <h1>{props.title}</h1>
     </div>
 	);
 }
 
+// Define Header component proptypes
 Header.propTypes = {
 	// Use isRequired function to check if property exists during runtime
 	title: React.PropTypes.string.isRequired,
+	players: React.PropTypes.array.isRequired,
 };
 
+// Define Counter component
 function Counter(props) {
 	return(
 		<div className="counter">
-	    <button className="counter-action decrement"> - </button>
+	    <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
 	    <div className="counter-score"> {props.score} </div>
-	    <button className="counter-action increment"> + </button>
+	    <button className="counter-action increment" onClick={function() {props.onChange(+1);}}> + </button>
 	  </div>
   );
 }
 
+// Define Counter component proptypes
 Counter.propTypes = {
 	score: React.PropTypes.number.isRequired,
+	onChange: React.PropTypes.func.isRequired,
 }
 
-// Add a Player component
+// Define Player component
 function Player(props) {
 	return(
 		<div className="player">
@@ -53,16 +86,18 @@ function Player(props) {
 	    	{props.name}
 	    </div>
 	    <div className="player-score">
-	        <Counter score={props.score}/>
+	        <Counter score={props.score} onChange={props.onScoreChange}/>
 	    </div>
 		</div>
 	);
 }
 
+// Define Player component proptypes
 Player.propTypes = {
 	// Use isRequired function to check if property exists during runtime
 	name: React.PropTypes.string.isRequired,
 	score: React.PropTypes.number.isRequired,
+	onScoreChange: React.PropTypes.func.isRequired,
 };
 
 // Define Application component class
@@ -92,16 +127,29 @@ var Application = React.createClass({
 		};
 	},
 
+	onScoreChange: function(index, delta) {
+		// Test onScoreChange
+		// console.log('onScoreChange', index, delta);
+		this.state.players[index].score += delta;
+		this.setState(this.state);
+	},
+
 	// Render the Application component
 	render: function() {
 		return (
 	    <div className="scoreboard">
-	      <Header title={this.props.title} />
+	      <Header title={this.props.title} players={this.state.players}/>
 
 	      <div className="players">
-          {this.state.players.map(function(player) {
-    				return <Player name={player.name} score={player.score} key={player.id} />
-    			})}
+          {this.state.players.map(function(player, index) {
+    				return (
+    					<Player 
+    					onScoreChange={function(delta) {this.onScoreChange(index, delta)}.bind(this)}
+    					name={player.name} 
+    					score={player.score} 
+    					key={player.id} />
+    				);
+    			}.bind(this))}
 	      </div>
 	    </div>
 		);
